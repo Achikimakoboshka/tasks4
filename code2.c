db428865a7b7e0130600dbf9892618e4b7f20344
unknown <achikimakobo17@gmail.com>
2021-07-22 01:32:07 +0300

    Вторая задача: ф-ия поиска макс из 4-х значений

---
+++ code2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int maxim(int a, int b, int c, int d);
+
+int main () {
+	int max_of_4 = maxim (3, 6, 2, 7);
+	printf ("%d\n", max_of_4);
+	return 0;
+}
---
+++ maxim.c
@@ -0,0 +1,18 @@
+int maxim(int a, int b, int c, int d) {
+
+	if (a > b && a > c && a > d) {
+		return a;
+	}
+
+	if (b > a && b > c && b > d) {
+		return b;
+	}
+
+	if (c > a && c > b && c > d) {
+		return c;
+	}
+	
+	if(d > a && d > b && d > c) { 
+		return d;
+	}
+}
